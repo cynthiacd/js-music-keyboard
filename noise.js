d5eb50d5701f5ca88d51750c7703147adab7aca7
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-05-22 16:12:37 -0700

    another way - using this and event - learned from Sahana

--- noise.js
+++ noise.js
@@ -1,28 +1,19 @@
 $(document).ready( function() {
 
-  $('.instrument').on('click', 'button', function(event) {
-    // $('button').append('<embed id="embed_player" src="audio.wav" autostart="true" hidden="true"></embed>');
-    var note = $(this).attr('class').slice(-1);
-    var audio = $('#' + note + 'Audio');
-    // console.log(audio);
+  $('button').click( function(event) {
+    var note = $(this).html();
+    var audio = note + 'Audio';
 
-    // who knows what get(0) is doing ...
-    // The get method returns the element itself, as an object. For example, we have this element:
-    // .get(0) will give you the first element in the jquery object,
-    audio.get(0).load();
-    audio.get(0).play();
-
-    // if you can access the html with elementById doesn't require the get(0).. to play
+    document.getElementById(audio).load();
+    document.getElementById(audio).play();
   });
 
   $('body').keydown( function(event) {
     // a through g letter keycodes fall between 65 through 71
-    if ( event.keyCode > 64 && event.keyCode < 72) {
-      var note = String.fromCharCode(event.keyCode).toLowerCase();
-      var audio = $('#' + note + 'Audio')
+    var note = event.key;
+    var audio = note + 'Audio';
 
-      audio.get(0).load();
-      audio.get(0).play();
-    }
+    document.getElementById(audio).load();
+    document.getElementById(audio).play();
   });
 });
