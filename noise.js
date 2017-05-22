b3d6ab7a563b7502a1bc7d5cb8fb2903f8e02632
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-05-22 14:59:35 -0700

    cleaned up code for keydown function

--- noise.js
+++ noise.js
@@ -1,47 +1,27 @@
 $(document).ready( function() {
 
-  // $('.instrument').on('click', 'button', function(event) {
-  //   // $('button').append('<embed id="embed_player" src="audio.wav" autostart="true" hidden="true"></embed>');
-  //   var note = $(this).attr('class').slice(-1);
-  //   // using elementById doesn't require the get(0)
-  //   var audio = $('#' + note + 'Audio')
-  //   console.log(audio);
-  //
-  //   // who knows what get(0) is doing ...
-  //   audio.get(0).load();
-  //   audio.get(0).play();
-  // });
+  $('.instrument').on('click', 'button', function(event) {
+    // $('button').append('<embed id="embed_player" src="audio.wav" autostart="true" hidden="true"></embed>');
+    var note = $(this).attr('class').slice(-1);
+    // using elementById doesn't require the get(0)..
+    var audio = $('#' + note + 'Audio')
+    console.log(audio);
 
-  $('body').keydown( function(event) {
-    switch (event.key) {
-      case 'c':
-        var audio = $('#cAudio');
-        break;
-      case 'd':
-        var audio = $('#dAudio');
-        break;
-      case 'e':
-        var audio = $('#eAudio');
-        break;
-      case 'f':
-        var audio = $('#fAudio');
-        break;
-      case 'g':
-        var audio = $('#gAudio');
-        break;
-      case 'a':
-        var audio = $('#aAudio');
-        break;
-      case 'b':
-        var audio = $('#bAudio');
-        break;
-    }
-
-    // if (event.key == 'c') {
-    //   var audio = $('#cAudio');
-    // } else if (event.key == 'd' ) {
-    // }
+    // who knows what get(0) is doing ...
     audio.get(0).load();
     audio.get(0).play();
   });
+
+  $('body').keydown( function(event) {
+
+    if ( event.keyCode > 64 && event.keyCode < 72) {
+      var note = String.fromCharCode(event.keyCode).toLowerCase();
+      var audio = $('#' + note + 'Audio')
+
+      audio.get(0).load();
+      audio.get(0).play();
+    }
+  });
 });
+
+// keycodes 65 through 71
