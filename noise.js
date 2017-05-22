6fd74a3eeb4369e11acda6037edda0e9b3e0286a
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-05-22 14:41:04 -0700

    working with typing keys and case statement

--- noise.js
+++ noise.js
@@ -1,36 +1,47 @@
 $(document).ready( function() {
 
-  $('.instrument').on('click', 'button', function(event) {
-    // $('button').append('<embed id="embed_player" src="audio.wav" autostart="true" hidden="true"></embed>');
-    var note = $(this).attr('class').slice(-1);
-    var audio = $('#' + note + 'Audio')
-    console.log(audio);
+  // $('.instrument').on('click', 'button', function(event) {
+  //   // $('button').append('<embed id="embed_player" src="audio.wav" autostart="true" hidden="true"></embed>');
+  //   var note = $(this).attr('class').slice(-1);
+  //   // using elementById doesn't require the get(0)
+  //   var audio = $('#' + note + 'Audio')
+  //   console.log(audio);
+  //
+  //   // who knows what get(0) is doing ...
+  //   audio.get(0).load();
+  //   audio.get(0).play();
+  // });
 
+  $('body').keydown( function(event) {
+    switch (event.key) {
+      case 'c':
+        var audio = $('#cAudio');
+        break;
+      case 'd':
+        var audio = $('#dAudio');
+        break;
+      case 'e':
+        var audio = $('#eAudio');
+        break;
+      case 'f':
+        var audio = $('#fAudio');
+        break;
+      case 'g':
+        var audio = $('#gAudio');
+        break;
+      case 'a':
+        var audio = $('#aAudio');
+        break;
+      case 'b':
+        var audio = $('#bAudio');
+        break;
+    }
+
+    // if (event.key == 'c') {
+    //   var audio = $('#cAudio');
+    // } else if (event.key == 'd' ) {
+    // }
     audio.get(0).load();
     audio.get(0).play();
   });
 });
-
-// $('#wrap').append('<embed id="embed_player" src="audio.wav" autostart="true" hidden="true"></embed>');
-
-
-//
-// var colors = ['blue', 'green', 'orange'];
-// var color_index = 0;
-//
-// var nextColor = function() {
-//   var color = colors[color_index];
-//   color_index++;
-//   color_index %= colors.length;
-//   return color;
-// };
-//
-// $(document).ready(function() {
-//   // Add code that uses the DOM here
-//   $("body").keydown( function(event) {
-//     // if you hit space bar in body - the box will change color
-//     // can also have event.key == "32"
-//     if (event.keyCode == 32){ $("#color-box").removeClass().addClass(nextColor()); }
-//   });
-//
-// });
